c20a245194d287cf64dec2d3d8865a1fa18eb484
Yoga Raditya <radityanalaa@gmail.com>
2024-05-31 07:42:03 +0700

    chore: Update query to fetch latest data in ascending sort

--- web/services/moistureService.js
+++ web/services/moistureService.js
@@ -1,14 +1,20 @@
 const pool = require("../database/database");
 
 async function insertMoistureData(data) {
-  const result = await pool.query('INSERT INTO moisture (controller_id, moisture) VALUES ($1, $2) RETURNING *', [data.controller_id, data.moisture]);
+  const result = await pool.query(
+    "INSERT INTO moisture (controller_id, moisture) VALUES ($1, $2) RETURNING *",
+    [data.controller_id, data.moisture]
+  );
   return result.rows[0];
-};
+}
 
 async function getAllMoistureData() {
-  const result = await pool.query('SELECT * FROM moisture ORDER BY timestamp ASC LIMIT 10');
+  const result = await pool.query(`
+  SELECT * FROM (SELECT * FROM moisture ORDER BY timestamp DESC LIMIT 10)
+  subquery ORDER BY timestamp ASC
+  `);
   return result.rows;
-};
+}
 
 module.exports = {
   insertMoistureData,
