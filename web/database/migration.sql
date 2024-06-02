70938dfd5170c96acc2f801c18f1d445caa2eb27
Yoga Raditya <radityanalaa@gmail.com>
2024-06-02 17:55:19 +0700

    chore: Add foreign key references

--- web/database/migration.sql
+++ web/database/migration.sql
@@ -6,3 +6,13 @@ CREATE TABLE moisture (
     moisture INT NOT NULL,
     timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE controller (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255)
+);
+
+ALTER TABLE moisture
+ADD CONSTRAINT fk_controller
+FOREIGN KEY (controller_id) REFERENCES controller(id);
