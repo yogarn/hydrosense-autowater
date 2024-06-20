0f32d84c1b6d81fa3aae85ebff1bd0f1b0f47884
Yoga Raditya <radityanalaa@gmail.com>
2024-06-20 10:35:05 +0700

    chore: Remove webserver and send data to the server instead

--- hydrosense-autowater.ino
+++ hydrosense-autowater.ino
@@ -1,92 +1,62 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <EEPROM.h>
-
-ESP8266WebServer server(80);
 
 #define AOUT_PIN A0   
 int THRESHOLD;
-
 #define THRESHOLD_EEPROM_ADDR 0
+
+const char* ssid = ""; // Replace with your WiFi SSID
+const char* password = ""; // Replace with your WiFi password
+
+WiFiClient client;
 
 void setup() {
   Serial.begin(9600);
   pinMode(D4, OUTPUT);
 
-  WiFi.softAP("ESP8266_AP", "password123");
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-
-  EEPROM.begin(512);
-  EEPROM.get(THRESHOLD_EEPROM_ADDR, THRESHOLD);
-  EEPROM.end();
-
-  server.on("/", HTTP_GET, handleRoot);
-
-  server.on("/moisture", HTTP_GET, handleMoisture);
-
-  server.on("/updateThreshold", HTTP_POST, updateThreshold);
-
-  server.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  server.handleClient();
-  
   static unsigned long lastCheckTime = 0;
   unsigned long currentTime = millis();
   if (currentTime - lastCheckTime >= 1000) {
-    handleMoisture();
+    sendMoistureData();
     lastCheckTime = currentTime;
   }
 }
 
-
-void handleRoot() {
-  String content = "<html><head><script>function refreshData() {var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {document.getElementById('moistureLevel').innerHTML = this.responseText;}}; xhttp.open('GET', '/moisture', true); xhttp.send();} setInterval(refreshData, 500);</script></head><body>";
-  content += "<h1>Soil Moisture Level</h1>";
-  content += "<p id='moistureLevel'>Loading...</p>";
-  
-  content += "<form action='/updateThreshold' method='post'>";
-  content += "Threshold: <input type='number' name='threshold' value='" + String(THRESHOLD) + "'><br>";
-  content += "<input type='submit' value='Update Threshold'>";
-  content += "</form>";
-
-  content += "</body></html>";
-
-  server.send(200, "text/html", content);
-}
-
-void handleMoisture() {
+void sendMoistureData() {
   int value = analogRead(AOUT_PIN);
-  
-  if (value > THRESHOLD) {
-    digitalWrite(D4, LOW);
-    server.send(200, "text/plain", "The soil is DRY (" + String(value) + ")");
-  }
-  else {
-    digitalWrite(D4, HIGH);
-    server.send(200, "text/plain", "The soil is WET (" + String(value) + ")");
-  }
-}
 
-void updateThreshold() {
-  if (server.hasArg("threshold")) {
-    int newThreshold = server.arg("threshold").toInt();
-    if (newThreshold >= 0 && newThreshold <= 1023) {
-      THRESHOLD = newThreshold;
-      
-      EEPROM.begin(512);
-      EEPROM.put(THRESHOLD_EEPROM_ADDR, THRESHOLD);
-      EEPROM.commit();
-      EEPROM.end();
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(client, "http://192.168.100.107:3000/moisture"); // Replace with your server endpoint
 
-      server.send(200, "text/plain", "Threshold updated successfully");
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonData = "{\"controller_id\": 1, \"moisture\": " + String(value) + "}";
+    int httpResponseCode = http.POST(jsonData);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
     } else {
-      server.send(400, "text/plain", "Invalid threshold value");
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
     }
+
+    http.end();
   } else {
-    server.send(400, "text/plain", "No threshold value provided");
+    Serial.println("WiFi Disconnected");
   }
 }
